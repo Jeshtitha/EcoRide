8497b9f33bf850ec4b0a07bc9a8983d7c2a631bc
divyalahari456 <divyalaharik@gmail.com>
2026-01-06 20:42:07 +0530

    API key added

---
+++ config.ts
@@ -0,0 +1,4 @@
+
+export const GEMINI_KEY = import.meta.env.VITE_GEMINI_API_KEY
+export const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL
+export const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY
